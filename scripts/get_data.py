14b9547e4a67cacd709aba9003b0c1f3f5a07dbc
Linus Moreau <linus.moreau@gmail.com>
2024-11-08 10:58:25 -0800

    Add script which gets the data and prints out one example of each type

---
+++ scripts/get_data.py
@@ -0,0 +1,35 @@
+# get_data.py
+
+import requests
+from google.transit import gtfs_realtime_pb2
+
+def get_data():
+    api_key = "MvqfD3MfvQlWaJI5fQeT"
+    trip_url = "https://gtfsapi.translink.ca/v3/gtfsrealtime?apikey=" + api_key
+    position_url = "https://gtfsapi.translink.ca/v3/gtfsposition?apikey=" + api_key
+    alerts_url = "https://gtfsapi.translink.ca/v3/gtfsalerts?apikey=" + api_key
+    
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get(trip_url)
+    if (response.ok):
+        feed.ParseFromString(response.content)
+        print("---------------- TRIP UPDATE ----------------")
+        print(feed.entity[0])
+        
+    response = requests.get(position_url)
+    if (response.ok):
+        feed.ParseFromString(response.content)
+        print("---------------- POSITION UPDATE ----------------")
+        print(feed.entity[0])
+        
+    response = requests.get(alerts_url)
+    if (response.ok):
+        feed.ParseFromString(response.content)
+        print("---------------- ALERTS ----------------")
+        print(feed.entity[0])
+        
+    return
+
+
+if __name__ == "__main__":
+    get_data()
