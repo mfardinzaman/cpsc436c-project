334495debfaf85ec70f6e33552a8bc003f2e8bee
Chinchin Jin <chinchinjin@host144-132.resnet.ubc.ca>
2024-11-19 16:32:36 -0800

    use mui-datatables; simple search/sort functionalities added

--- frontend/src/components/RouteDashboard.js
+++ frontend/src/components/RouteDashboard.js
@@ -1,122 +1,80 @@
-import React, { useEffect, useState } from "react";
-import mockRoutes from '../mock/mockRoutes.json';
-import mockBuses from '../mock/mockVehicles.json'
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    IconButton,
-    Collapse,
-    Typography,
-    Box,
-    Paper,
-    Card,
-} from "@mui/material";
-import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
+import React, { useState } from "react";
+import MUIDataTable from "mui-datatables";
+import mockRoutes from "../mock/mockRoutes.json";
+
+import { Typography, Box } from "@mui/material";
+import RouteExpandableRow from "./RouteExpandableRow";
 
 const RouteDashboard = () => {
-    const [routes, setRoutes] = useState(mockRoutes);
+    const [routes] = useState(mockRoutes);
+
+    // Define table columns
+    const columns = [
+        {
+            name: "route_short_name",
+            label: "Route Short Name",
+        },
+        {
+            name: "route_long_name",
+            label: "Route Long Name",
+        },
+        {
+            name: "numBuses",
+            label: "Number of Buses",
+            options: {
+                searchable: false
+            }
+        },
+        {
+            name: "avgLateness",
+            label: "Average Lateness (min)",
+            options: {
+                searchable: false
+            }
+        },
+        {
+            name: "latePercentage",
+            label: "% > 5 Min Late",
+            options: {
+                searchable: false
+            }
+        },
+    ];
+
+    const options = {
+        selectableRows: "none",
+        expandableRows: true,
+        expandableRowsHeader: false,
+        renderExpandableRow: (rowData, rowMeta) => (
+            <RouteExpandableRow
+                rowData={rowData}
+                rowMeta={rowMeta}
+                routes={routes}
+            />
+        ),
+        search: true,
+        print: false,
+        download: false,
+        filter: false
+    };
 
     return (
-        <TableContainer component={Card} style={{
-            width: '80%',
-            margin: '20px auto',
-            padding: '10px'
-          }}>
-            <Typography variant='h4' align='left'>Routes</Typography>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell />
-                        <TableCell>Route Name</TableCell>
-                        <TableCell align="center">Number of Buses</TableCell>
-                        <TableCell align="center">Average Lateness (min)</TableCell>
-                        <TableCell align="center">% 5 Min Late</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {routes.map((route) => (
-                        <React.Fragment key={route.routeId}>
-                            {/* Main Row */}
-                            <Row route={route} />
-                        </React.Fragment>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Box
+            sx={{
+                margin: "20px auto",
+                padding: 2,
+            }}
+        >
+            <Typography variant="h4">
+                Routes Dashboard
+            </Typography>
+            <MUIDataTable
+                data={routes}
+                columns={columns}
+                options={options}
+            />
+        </Box>
     );
 };
-
-const Row = ({ route }) => {
-    const [open, setOpen] = useState(false)
-    const [buses, setBuses] = useState(mockBuses)
-    const toggleExpand = () => {
-        setOpen(!open)
-    }
-
-    return (
-        <>
-            <TableRow>
-                <TableCell>
-                    <IconButton
-                        onClick={toggleExpand}
-                        aria-label="expand row"
-                        size="small"
-                    >
-                        {open ? (
-                            <KeyboardArrowUp />
-                        ) : (
-                            <KeyboardArrowDown />
-                        )}
-                    </IconButton>
-                </TableCell>
-                <TableCell>{`${route.route_short_name} ${route.route_long_name}`}</TableCell>
-                <TableCell align="center">...</TableCell>
-                <TableCell align="center">...</TableCell>
-                <TableCell align="center">...</TableCell>
-            </TableRow>
-            {open &&
-                <TableRow>
-                    <TableCell colSpan={5}>
-                        <Collapse in={open} timeout="auto" unmountOnExit>
-                            <Box margin={2}>
-                                <Typography variant="h6">
-                                    Buses on {route.route_short_name}
-                                </Typography>
-                                <Table size="small">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>Label</TableCell>
-                                            <TableCell>Status</TableCell>
-                                            <TableCell>Lateness (min)</TableCell>
-                                            <TableCell>Last Stop</TableCell>
-                                            <TableCell>Last Update</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {buses.map((bus) => (
-                                            <TableRow key={bus.vehicle_id}>
-                                                <TableCell>{bus.vehicle_id}</TableCell>
-                                                <TableCell>...</TableCell>
-                                                <TableCell>...</TableCell>
-                                                <TableCell>{bus.update_time}</TableCell>
-                                                <TableCell>
-                                                    {new Date(bus.update_time).toLocaleString()}
-                                                </TableCell>
-                                            </TableRow>
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </Box>
-                        </Collapse>
-                    </TableCell>
-                </TableRow>
-            }
-        </>
-    )
-}
 
 export default RouteDashboard;
