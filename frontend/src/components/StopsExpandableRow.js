59f33968f0087030f3f41ccf01d6bbde35733d95
TonyZhang1112 <tonyzhang2187@gmail.com>
2024-11-30 15:27:47 -0800

    feat: Add vehicle route and direction to StopsExpandableRow

--- frontend/src/components/StopsExpandableRow.js
+++ frontend/src/components/StopsExpandableRow.js
@@ -99,12 +99,16 @@ const StopsExpandableRow = ({ rowData, stop }) => {
                             title={`Updates at Stop: ${stop.stop_name}`}
                             data={updates.map((update) => ({
                                 label: update.vehicle_label,
+                                number: update.route_short_name,
+                                direction: update.direction_name,
                                 lateness: update.delay / 60,
                                 stopTime: convertUnixTimeToPST(moment.utc(update.stop_time).valueOf()),
                                 lastUpdate: convertUnixTimeToPST(moment.utc(update.update_time).valueOf()),
                             }))}
                             columns={[
                                 { name: "label", label: "Vehicle Label" },
+                                { name: "number", label: "Number" },
+                                { name: "direction", label: "Direction" },
                                 {
                                     name: "lateness",
                                     label: "Delay (min)",
